4aadcff334c4b6a1deec62ce4fb1fc48d76abf7a
zhongyi li <llzzyy1005@163.com>
2021-09-24 17:45:50 +1200

    test

---
+++ precache-manifest.ff935e1be41438eff4590f684b6c4291.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b165f08410270b00c11a79bea2d918",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9f335583fd80c254012",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "533ed47da5eb89493816",
+    "url": "/static/css/main.aa09dc64.chunk.css"
+  },
+  {
+    "revision": "a9f335583fd80c254012",
+    "url": "/static/js/2.f0456874.chunk.js"
+  },
+  {
+    "revision": "533ed47da5eb89493816",
+    "url": "/static/js/main.2da8e7e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6dd31e94697146d3e615dcb181e640c",
+    "url": "/static/media/card_home_1.d6dd31e9.jpg"
+  },
+  {
+    "revision": "0e6bbf23baa10a8aa4836247da616fbc",
+    "url": "/static/media/card_home_2.0e6bbf23.jpg"
+  },
+  {
+    "revision": "2c501851c47775a35d3425d8faec8775",
+    "url": "/static/media/card_home_3.2c501851.jpg"
+  },
+  {
+    "revision": "0630472dec696f50f64b570a7c733e05",
+    "url": "/static/media/home_kitchen.0630472d.jpg"
+  },
+  {
+    "revision": "614391b94502c82caaa482aea181243c",
+    "url": "/static/media/home_sitting.614391b9.jpg"
+  },
+  {
+    "revision": "ad45fbbc2d151b97efb37ac825d66b7c",
+    "url": "/static/media/home_top.ad45fbbc.jpg"
+  },
+  {
+    "revision": "a49fa35a5b3feee26ad9e9f1eca8cd3d",
+    "url": "/static/media/menu_top.a49fa35a.jpg"
+  }
+]);
