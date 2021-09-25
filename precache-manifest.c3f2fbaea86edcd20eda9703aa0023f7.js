588cdaf6b6f432838b8812fee2e9a133bb75dd21
zhongyi li <llzzyy1005@163.com>
2021-09-25 15:16:47 +1200

    test new

--- precache-manifest.c3f2fbaea86edcd20eda9703aa0023f7.js
+++ precache-manifest.c3f2fbaea86edcd20eda9703aa0023f7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b8c587ec772ae7af51cc61abfb6849f6",
+    "revision": "6c2f2d2e46aedb02883c3b1a5b05d4c0",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "f500f4a479aa165e0a89",
+    "revision": "b3192c440bab12cc7700",
     "url": "/static/css/main.aa09dc64.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.40d45173.chunk.js"
   },
   {
-    "revision": "f500f4a479aa165e0a89",
-    "url": "/static/js/main.5cc88a55.chunk.js"
+    "revision": "b3192c440bab12cc7700",
+    "url": "/static/js/main.da1fabd8.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.169a8a2c5dd655248f105c5b3022a041.js"
+  "/precache-manifest.c3f2fbaea86edcd20eda9703aa0023f7.js"
 );
 
 self.addEventListener('message', (event) => {
